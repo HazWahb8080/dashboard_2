bf753ddc8d058d930360652b1e9968e6524a38cc
HazWahb8080 <hazem.wahb100@gmail.com>
2022-08-10 14:10:31 +0200

    navbar to the end of the screen

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App overflow-y-hidden ">
      <Header/>
-     <div className='w-full min-h-[90vh] grid grid-cols-12 place-items-start '>
+     <div className='w-full min-h-[90vh] grid grid-cols-12'>
      <NavBar/>
      <div className='grid grid-cols-1 xl:grid-cols-5 col-span-10 w-full'>
       <LeftPart/>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -5,7 +5,7 @@ import { activeNavItemState } from "../../atoms/ActiveNavBarAtom";
 
 function NavBar() {
   return (
-    <nav className=" col-span-2 border-r border-gray-200 min-h-[90vh] w-[80px] xl:w-[250px] pt-8 px-1 flex flex-col items-start justify-between">
+    <nav className="col-span-2 border-r border-gray-200 min-h-[90vh] w-[80px] xl:w-[250px] pt-8 px-1 flex flex-col items-start justify-between">
       <div className="space-y-8 w-full ">
         {navLinks.slice(0, 4).map((link) => (
           <NavItem link={link} key={link.id} />
